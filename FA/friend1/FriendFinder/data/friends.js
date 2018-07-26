a32bd54773c4e7f1e99b9d2bef9c285a5ed463a0
tinatj <kod4er@gmail.com>
2018-07-26 16:07:22 -0600

    new

---
+++ FA/friend1/FriendFinder/data/friends.js
@@ -0,0 +1,126 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Mohamed",
+      photo: "https://www.google.com/search?q=orange+cat&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj0ou-r3L3cAhVM4oMKHcyiDo8Q_AUICigB&biw=1120&bih=642&dpr=1.25#imgrc=tGC6o1kbxaeTfM:",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "CAT2",
+      photo: "https://www.findcatnames.com/orange-cat-names-90-colorful-ideas/",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "catFriend3",
+      photo: "https://www.pinterest.ca/pin/19914423328555406/",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "cat4",
+      photo: "https://www.pinterest.ca/explore/red-cat/",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "FriendCats5",
+      photo: "https://www.pinterest.ca/explore/cats-and-kittens/",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "cat6",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+        name: "cat7",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwje_ruD3r3cAhXEmuAKHXzaCnoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F470978073512858250%2F&psig=AOvVaw0eNTCKMxMdtDdE6tmqi5xA&ust=1532727446118575",
+        scores: [
+          "4",
+          "4",
+          "2",
+          "3",
+          "2",
+          "2",
+          "3",
+          "2",
+          "4",
+          "5"
+        ]
+      }, 
+
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
